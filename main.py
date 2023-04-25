f5db3b9d1e006be37c7f9c96d878b69662dafbe3
ekinbayri <ekinbayri@gmail.com>
2023-04-25 15:20:44 +0300

    Updated functionality

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ f = open(FILE_LR, "r")
 data = f.readline()
 data = data.strip()
 data = data.split(";")
-multiplier = len(data) - 1
+
 variables = f.readline()
 variables = variables.split(";")
 for i in range(len(variables)):
@@ -21,72 +21,82 @@ f2.readline()
 states = f2.readlines()
 f2.close()
 loopBool = True
-loopVar = 0
+
+
 f3 = open(FILE_INPUT, "r")
 inputs = f3.readlines()
 f3.close()
-currentInputLine = inputs[2].split()
-currentInput = currentInputLine[1]
-currentInput = currentInput[1:]
-print("Processing input string", currentInput, "for LR(1) parsing table.")
-print("NO    |    STATE STACK    |    READ    |    INPUT    |    ACTION")
-indexer = 0
-stateDict = {"blank": 1}
-stateStack = [1]
-counter = 1
-while loopBool:
-    currentState = states[loopVar].split(";")
-    currentVariable = currentInput[indexer]
-    print(counter,"    |", stateStack, (" " * (16 - (len(stateStack) * 3))), "|     ", currentVariable, "    |    ", currentInput,
-          (" " * (4 - (len(currentInput)))), "  | ", end=" ")
-    indexer += 1
-    selector = -1
-    counter += 1
-    for i in range(len(variables)):
-        if variables[i] == currentVariable:
-            selector = i + 1
-            break
-    if currentState[selector] == "       ":
-        print("REJECTED (", currentState[0], "does not have action/step for", currentVariable, ")")
-        loopBool = False
+
+
+for i in range(len(inputs)):
+    currentInputLine = inputs[i].split()
+    if currentInputLine[0] == "LR":
+        currentInput = currentInputLine[1]
+        currentInput = currentInput[1:]
+        loopBool = True
     else:
-
-        if currentState[selector].rfind("_") != -1:
-            currentState[selector] = currentState[selector][currentState[selector].rfind("_") + 1:]
-            loopVar = int(currentState[selector]) - 1
-            stateDict[currentVariable] = loopVar + 1
-            stateStack.append(loopVar + 1)
-            print("Shift to", currentState[selector])
-        elif currentState[selector] == "Accept":
-            print("ACCEPTED")
-            break
-        else:
-            rightSide = currentState[selector][currentState[selector].rfind("->") + 2:]
-            rightSide = rightSide.strip()
-
-            leftSide = currentState[selector][0:currentState[selector].rfind("->")]
-            print("Reverse", leftSide, "->", rightSide)
-            index = currentInput.rfind(rightSide)
-            if index != -1:
-                currentInput = currentInput.replace(rightSide, leftSide)
-                if len(rightSide) == 1:
-                    stateDict.pop(rightSide)
-                    stateStack.pop()
-                else:
-                    for i in rightSide:
-                        stateDict.pop(i)
-                        stateStack.pop()
-
-                indexer = currentInput.rfind(leftSide)
-
-                loopVar = stateStack.pop() - 1
-                stateStack.append(loopVar + 1)
-
-            else:
-                print("REJECTED (can't apply rule)")
-
+        loopBool = False
+    if loopBool:
+        print("\nProcessing input string", currentInput, "for LR(1) parsing table.")
+        print("NO    |    STATE STACK    |    READ    |    INPUT    |    ACTION")
+    indexer = 0
+    stateDict = {"blank": 1}
+    stateStack = [1]
+    counter = 1
+    loopVar = 0
+    while loopBool:
+        currentState = states[loopVar].split(";")
+        currentVariable = currentInput[indexer]
+        print(counter,"    |", stateStack, (" " * (16 - (len(stateStack) * 3))), "|     ", currentVariable, "    |    ", currentInput,
+              (" " * (4 - (len(currentInput)))), "  | ", end=" ")
+        indexer += 1
+        selector = -1
+        counter += 1
+        for i in range(len(variables)):
+            if variables[i] == currentVariable:
+                selector = i + 1
                 break
- 
+        if currentState[selector] == "       ":
+            print("REJECTED (", currentState[0], "does not have action/step for", currentVariable, ")")
+            loopBool = False
+        else:
+
+            if currentState[selector].rfind("_") != -1:
+                currentState[selector] = currentState[selector][currentState[selector].rfind("_") + 1:]
+                loopVar = int(currentState[selector]) - 1
+                stateDict[currentVariable] = loopVar + 1
+                stateStack.append(loopVar + 1)
+                print("Shift to", currentState[selector])
+            elif currentState[selector] == "Accept":
+                print("ACCEPTED")
+                break
+            else:
+                rightSide = currentState[selector][currentState[selector].rfind("->") + 2:]
+                rightSide = rightSide.strip()
+
+                leftSide = currentState[selector][0:currentState[selector].rfind("->")]
+                print("Reverse", leftSide, "->", rightSide)
+                index = currentInput.rfind(rightSide)
+                if index != -1:
+                    currentInput = currentInput.replace(rightSide, leftSide)
+                    if len(rightSide) == 1:
+                        stateDict.pop(rightSide)
+                        stateStack.pop()
+                    else:
+                        for i in rightSide:
+                            stateDict.pop(i)
+                            stateStack.pop()
+
+                    indexer = currentInput.rfind(leftSide)
+
+                    loopVar = stateStack.pop() - 1
+                    stateStack.append(loopVar + 1)
+
+                else:
+                    print("REJECTED (can't apply rule)")
+
+                    break
+
 
 # LL part
 
@@ -105,7 +115,7 @@ def parseOperators(FILE_LL):
     for i in range(1, len(ops)):
         operator = ops[i].replace(" ", "")
         operators.append(operator)
-    
+
     return operators
 
 def parseActions(FILE_LL):
@@ -126,16 +136,15 @@ def parseTable(FILE_LL):
     for i in range(len(llData)):
         llData[i] = llData[i].rstrip()
         llData[i] = llData[i].split(";")
-    
+
     operatorList = []
     # prepare table as dict e.g. Eid = E -> TA 
     for i in range(1, len(llData)):
         for j in range(1, len(llData[0])):
             if((not llData[i][j].isspace()) and llData[i][j] != ""):
                 tableDict[(llData[i][0] + llData[0][j]).replace(" ", "")] = llData[i][j].strip()
-    
-    return tableDict
 
+    return tableDict
 
 
 def derivate(tableDict, inputs, operators, actions):
@@ -149,28 +158,27 @@ def compare(smaller, bigger, numberOfChar):
         if(smaller[i] != bigger[i]):
             return False
     return True
-         
+
 
 def ll(tableDict, input, operators, actions):
     no = 0
     stack = []
     stack.append('$')
-    
-    print(no)
-    print(stack)
-    print(input)
+
+
     maxOperatorLength = 0
     for i in range(len(operators)):
         if((currentOpLen := len(operators[i])) > maxOperatorLength):
             maxOperatorLength = currentOpLen
-    
+
     loopBool = True
+    print()
+    print("NO    |          STACK          |    INPUT    |    ACTION")
     while loopBool:
         no += 1
-        print(no)
-        print(stack)
-        print(input)
-        
+        print(no, "    |", stack, (" " * (20 - (len(stack) * 5))), "|     ", input,(" " * (4 - (len(input)))), "  | ", end=" ")
+
+
         # checking input value to validate
         while True:
             if (no != 1):
@@ -204,30 +212,29 @@ def ll(tableDict, input, operators, actions):
                     return
                 else:
                     stackV += stack.pop()
-                
+
             if(inpV in operators and stackV in operators):
                 input = input[len(inpV):]
             else:
                 break
-        
-        
+
 
         if(inpV in operators and stackV in operators and inpV != stackV):
             print("rejected")
             return
-    
-            
+
+
         if(inpV in operators and stackV in actions):
             dictKey = stackV + inpV
             if(dictKey not in list(tableDict.keys())):
                 print("reject")
                 break
             action = tableDict[dictKey]
-            
+
             print(action)
             actionD = action.split("->")[1]
             if(actionD != "ϵ"):
-                
+
                 actionList = []
                 for i in range(len(actionD)):
                     if(actionD[i] in actions or actionD[i] in operators):
